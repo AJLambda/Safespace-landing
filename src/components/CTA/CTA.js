99a262ffff83f051c3ac5af4f13723b1a9522e28
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-25 00:42:29 -0700

    carousel looking goood

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -1,40 +1,57 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { Button } from "reactstrap";
 import "./CTA.scss";
 
 export default function CTA() {
   return (
     <Carousel
       infiniteLoop
-      autoPlay
+      // autoPlay
       showArrows={true}
       showStatus={false}
       showThumbs={false}
       interval={5000}
       transitionTime={1200}
       stopOnHover={false}
-
-      // onChange={onChange}
-      // onClickItem={onClickItem}
-      // onClickThumb={onClickThumb}
     >
-      <div>
-        <img src="http://cdn.filestackcontent.com/2ttboiItRVWZK0iEg5d6" />
-        {/* <p className="legend">Legend 3</p> */}
+      <div className="slide-wrap" id="one">
+        <div className="slide-content">
+          <h1>Step By Step Programs</h1>
+          <p>
+            Our step by step programs provide you with information and tools to
+            have peace of mind
+          </p>
+          <Button className="cta-btn">Sign up</Button>
+        </div>
       </div>
-      <div>
-        <img src="http://cdn.filestackcontent.com/2ElEEBCsSxqFVBoGS07w" />
-        {/* <p className="legend">Legend 1</p> */}
+      <div className="slide-wrap" id="two">
+        <div className="slide-content">
+          <h1>Evidence-Based Psychology</h1>
+          <p>
+            Safespace uses evidence-based psychology to help you tackle mental
+            health challenges and develop your mental skills
+          </p>
+          <Button className="cta-btn">Sign up</Button>
+        </div>
       </div>
-      <div>
-        <img src="http://cdn.filestackcontent.com/3VjQXokmQO6Yfh3rRjfN" />
-        {/* <p className="legend">Legend 2</p> */}
+      <div className="slide-wrap" id="three">
+        <div className="slide-content">
+          <h1>Mental Health Check</h1>
+          <p>Everbody has mental health, click here to learn yours</p>
+          <Button className="cta-btn">Sign up</Button>
+        </div>
       </div>
-
-      <div>
-        <img src="http://cdn.filepicker.io/api/file/OmLnxDmJRSqf5zznZxgu" />
-        {/* <p className="legend">Legend 4</p> */}
+      <div className="slide-wrap" id="four">
+        <div className="slide-content">
+          <h1>Sadness & Depression</h1>
+          <p>
+            Work through unwanted thoughts and behaviors by creating healthy
+            habits with proven techniques
+          </p>
+          <Button className="cta-btn">Sign up</Button>
+        </div>
       </div>
     </Carousel>
   );
