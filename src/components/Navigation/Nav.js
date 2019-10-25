af33de348f4c62c44b467e20d96c60993eaa7bbe
Lambda_School_Loaner_191 <lambda_school_loaner_191@lambdasr191smbp.home>
2019-10-24 19:45:07 -0700

    added logo to nav

---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button
+} from "reactstrap";
+import NavLogo from "../Images/safespacelogo1.png";
+import "./Nav.scss";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className="navbar" color="light" light expand="md">
+        <NavbarBrand href="/">
+          <img
+            src={NavLogo}
+            width="50"
+            height="50"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          <div className="nav-title-wrapper">
+            <div className="nav-title">Safespace</div>
+            <div className="nav-undertitle">
+              building healthy minds and hearts
+            </div>
+          </div>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Sign in</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://safespace-frontend.netlify.com/">
+                <Button color="primary">Sign up</Button>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
